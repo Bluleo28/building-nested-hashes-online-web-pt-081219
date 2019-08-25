6c4f81efb97d7b389063e8554f0359d9a5e6c11d
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-25 23:30:30 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,16 @@
 def second_challenge(family_names)
-   epic_tragedy = {
-     :montague => {
-       :name => :age
-       
-     },
-     :capulet => {
-       :name => :age
-  }
+  epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
 }
 end
