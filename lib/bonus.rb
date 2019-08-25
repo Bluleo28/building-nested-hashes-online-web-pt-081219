ecd2afd9c56b26cd9609c4abfc29eb1d88be9c15
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-25 23:36:48 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -12,18 +12,11 @@ def bonus
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine => {name: "Juliet", age: "15", status: "dead"},
       :heroine_friends => [
           {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
   }
-  end
-end
-def epic_tragedy = {
-  :montague => { hero {name: "Romeo", age: "17", attitude: "worried"},{{ :heroine => name: "Juliet", age: "15", status: "alive"}
-}
-  epic_tragedy
-  end
 end
